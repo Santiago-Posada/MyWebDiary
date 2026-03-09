0475f04fbe3d2c4e0d7567d83d0e87ab80bc36fd
unknown <santiagoposadaserna02@gmail.com>
2026-03-09 18:52:22 -0500

    fix env file and verify server running correctly

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,5 +3,7 @@ dotenv.config();
 
 import express from 'express';
 const app = express();
+
 app.get("/health",(req,res)=> res.json({status: "ok"}))
+
 app.listen(process.env.PORT, ()=>console.log(`Server running on port ${process.env.PORT}`));
