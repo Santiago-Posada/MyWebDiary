9256bbc482e009a2f9b726634ca788bd591ed015
unknown <santiagoposadaserna02@gmail.com>
2026-03-09 00:53:47 -0500

    add dotenv config and environment variable for port

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,4 +1,7 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
 import express from 'express';
 const app = express();
 app.get("/health",(req,res)=> res.json({status: "ok"}))
-app.listen(5000, ()=>console.log("Server running in port 5000"));
+app.listen(process.env.PORT, ()=>console.log(`Server running on port ${process.env.PORT}`));
